4329ee283a41848da4decf521990ac4e70032178
Claudio Domene <claudio79jr@gmail.com>
2019-06-12 15:21:17 -0300

    atualizacoes

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -10,6 +10,16 @@ public class BallScript : MonoBehaviour {
         myRb = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate() {
+        Debug.Log(myRb.velocity);
+
+        if (myRb.velocity.x > 6f)
+            myRb.velocity = new Vector2(6f, myRb.velocity.y);
+
+        if (myRb.velocity.y > 10f)
+            myRb.velocity = new Vector2(myRb.velocity.x, 10f);
+    }
+
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.tag == "Player") {
             float kickForce = other.gameObject.GetComponent<PlayerScript>().kickForce;
@@ -19,6 +29,14 @@ public class BallScript : MonoBehaviour {
             difference = difference.normalized * kickForce;
 
             myRb.AddForce(difference, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            float kickForce = other.gameObject.GetComponent<PlayerScript>().kickForce;
+
+            myRb.AddForce(Vector2.up * kickForce, ForceMode2D.Impulse);
         }
     }
 }
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
     public float speed = 5f;
 
-    public float jumpForce = 0.1f;
+    public float jumpForce = 5f;
 
     public float kickForce = 5f;
 
     private Rigidbody2D myRb;
 
+    private bool canJump = true;
+
     void Start() {
         myRb = GetComponent<Rigidbody2D>();
     }
     
-    void Update() {
+    void FixedUpdate() {
         if (Input.GetAxis("Horizontal") != 0) {
             transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
         }
         
-        if(Input.GetAxis("Vertical") > 0) {
-            myRb.AddForce(Vector2.up * jumpForce);
+        if(Input.GetAxis("Vertical") > 0 && canJump) {
+            myRb.AddForce(Vector2.up * jumpForce * 1000000);
+
+            canJump = false;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Ground")
+            canJump = true;
     }
 }
